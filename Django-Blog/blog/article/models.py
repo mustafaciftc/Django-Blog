07611f2aab69b08a9cd52f4e2604898bb2080b2a
Mustafa Çiftci <123982648+mustafaciftc@users.noreply.github.com>
2023-05-26 12:44:36 +0300

    Add files via upload

---
+++ Django-Blog/blog/article/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from ckeditor.fields import RichTextField
+
+class Article(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = RichTextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.FileField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+
